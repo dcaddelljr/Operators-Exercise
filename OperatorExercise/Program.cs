d46bbcbc703c7d95e6313760df880a5f36ee73f9
Dwight Caddell <dcaddelljr@gmail.com>
2023-03-14 15:27:58 -0500

    OperatorsExercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -23,34 +23,35 @@ namespace OperatorExercise
             var mod = a % b;
             Console.WriteLine($" modulus: {mod}\n");
 
-          var quotient = a / b;
+            var quotient = a / b;
             var remainder = a % b;
 
             Console.WriteLine($" {a}/{b} is {quotient} remainder {remainder}. \n");
 
             if (a == 17 && b == 4)
 
-            Console.WriteLine ("17/4 is 4 remainder 1.");
+                Console.WriteLine("17/4 is 4 remainder 1.");
 
-           //Exercise 2
-           Console.WriteLine("EXERCISE 2:");
+            //Exercise 2
+            Console.WriteLine("EXERCISE 2:");
 
 
-           Console.WriteLine("What is the radius of your circle?");
-           var radius = double.Parse(Console.ReadLine());
-
-            var area = Math.PI * Math.Pow(radius, 2);
-            
+            Console.WriteLine("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
 
             Console.WriteLine($"The area of a circle with radius of {radius} is {area} ");
 
             //Thought Exercise:
-            var i = 3;
-            var j = 4;
-            var k = ++i * j++;
+            int i = 3;
+            int j = 4;
+            int k = ++i * j++;
 
             Console.WriteLine(k);
-
         }
+            public static double AreaOfCircle(double r)
+            {
+                return Math.PI * r * r;
+            }
     }
 }
